6242d9dbe1a4b14ec5a074fc1219d9b0d6994c31
Pavel Ivanchenko <IvanchenkoPavel1186@gmail.com>
2013-03-25 17:45:18 +0200

    add RSpecs for  renew SSL

--- app/controllers/opensrs_controller.rb
+++ app/controllers/opensrs_controller.rb
@@ -10,7 +10,17 @@ class OpensrsController < ApplicationController
     puts xml
     # render "layouts/_get_order_info", :formats => [:xml], :header => {'sss' => 'ddd'}
     # render "layouts/_get_parse_csr", :formats => [:xml], :header => {'sss' => 'ddd'}
-    render "layouts/_get_trust_service_order", :formats => [:xml], :header => {'sss' => 'ddd'}
+    # render "layouts/_get_trust_service_order", :formats => [:xml], :header => {'sss' => 'ddd'}
+
+    # renew ssl templates
+    # render "layouts/_get_renewal_order_for_a_quickssl_certificate", :formats => [:xml], :header => {'sss' => 'ddd'}
+    # render "layouts/_get_an_order_to_upgrade_a_sitelock_ssl_certificate_to_sitelock_premium", :formats => [:xml], :header => {'sss' => 'ddd'}
+    # render "layouts/_get_an_order_for_a_geotrust_web_site_anti_malware_scan_certificate", :formats => [:xml], :header => {'sss' => 'ddd'}
+    # render "layouts/_get_an_order_for_a_30_day_free_trial_of_a_symantec_securesite_certificate", :formats => [:xml], :header => {'sss' => 'ddd'}
+    # render "layouts/_get_a_new_order_for_a_quickssl_certificate_based_on_an_existing_order", :formats => [:xml], :header => {'sss' => 'ddd'}
+    # render "layouts/_get_a_renewal_order_for_a_quickssl_certificate_that_was_submitted_by_using_the_order_id", :formats => [:xml], :header => {'sss' => 'ddd'}
+    render "layouts/_get_a_renewal_order_for_a_quickssl_certificate_that_was_submitted_by_using_the_product_id", :formats => [:xml], :header => {'sss' => 'ddd'}
+
   end
 
 end
