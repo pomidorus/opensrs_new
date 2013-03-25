c8620c04bbbd7ed8f76475c7808f0941de72124d
andrus <andrewaka39@gmail.com>
2013-03-25 18:05:38 +0200

    client opensrs

--- lib/client/client.rb
+++ lib/client/client.rb
@@ -50,36 +50,6 @@ rpi = opensrs_request.request_order_info("99")
 #puts request.response
 puts roi.request_xml
 puts rpi.request_xml
-
 #puts request.response_xml
 
 
-#server = OpenSRS::Server.new(
-#  #:server   => "http://opensrs.herokuapp.com/opensrs",
-#  :server   => "http://localhost:3000/opensrs",
-#  :username => "aseleznov",
-#  :password => "53cr3t",
-#  :key      => "c633be3170c7fb3fb29e2f99b84be2410"
-#)
-
-#request_order_info = server.call(
-#      :action => "GET_ORDER_INFO",
-#      :object => "DOMAIN",
-#      :attributes => {
-#        :order_id => "3515690"
-#      }
-#    )
-#
-#request_product_info = server.call(
-#      :action => "GET_ORDER_INFO",
-#      :object => "DOMAIN",
-#      :attributes => {
-#        :order_id => "3515690"
-#      }
-#    )
-
-
-#puts request.inspect
-#puts request.response
-#puts request.request_xml
-#puts request.response_xml
