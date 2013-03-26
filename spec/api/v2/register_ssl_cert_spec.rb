32bd590df5886f73122316a3eff848353b929bbf
Pavel Ivanchenko <IvanchenkoPavel1186@gmail.com>
2013-03-26 19:20:36 +0200

    update OpenSRS controller

--- spec/api/v2/register_ssl_cert_spec.rb
+++ spec/api/v2/register_ssl_cert_spec.rb
@@ -75,7 +75,10 @@ describe "/opensrs" do
         opensrs_request["action"].should == "GET_ORDER_INFO"
       end
 
-      it "object is TRUST_SERVICE"
+      it "object is TRUST_SERVICE" do
+        opensrs_request = OpenSRSRequest.new(get_register_ssl_cert("327").request_xml).request_hash
+        opensrs_request["object"].should == "TRUST_SERVICE"
+      end
 
       it "order_id is 327" do
         opensrs_request = OpenSRSRequest.new(get_register_ssl_cert("327").request_xml).request_hash
