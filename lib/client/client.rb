a5d7838dbe91114f77b563b75d7f0a4e083ee84b
andrus <andrewaka39@gmail.com>
2013-03-30 12:47:10 +0200

    order_info

--- lib/client/client.rb
+++ lib/client/client.rb
@@ -76,6 +76,31 @@ end
 opensrs_request = OpenSRSRequest.new("http://localhost:3000/opensrs","aseleznov","53cr3t","c633be3170c7fb3fb29e2f99b84be2410")
 #opensrs_request = OpenSRSRequest.new("http://opensrs.herokuapp.com/opensrs","aseleznov","53cr3t","c633be3170c7fb3fb29e2f99b84be2410")
 
+#getOrderInfo
+#getOrderInfo = {action: "GET_ORDER_INFO", object: "DOMAIN", order_id: "1234"}
+#getProductInfo = {action: "GET_PRODUCT_INFO", object: "TRUST_SERVICE", product_id: "1234"}
+#approverList = {action: "QUERY_APPROVER_LIST", object: "TRUST_SERVICE", domain: "www.mail.ru", product_type: "quickssl"}
+#resendApprEmail = {action: "RESEND_APPROVE_EMAIL", object: "TRUST_SERVICE", order_id: "123432"}
+#resendSertEmail = {action: "RESEND_CERT_EMAIL", object: "TRUST_SERVICE", order_id: "123432"}
+#cancelSSL = {action: "CANCEL_ORDER", object: "TRUST_SERVICE", order_id: "123432"}
+#
+#sslcert = %q(-----BEGIN CERTIFICATE REQUEST----- MIIBqTCCARICAQAwaTELMAkGA1UEBhMCQ0ExCzAJBgNVBAgTAm9uMRAwDgYDVQQH
+#Ewd0b3JvbnRvMQ8wDQYDVQQKEwZ0dWNvd3MxCzAJBgNVBAsTAnFhMR0wGwYDVQQD
+#ExR3d3cucWFyZWdyZXNzaW9uLm9yZzCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkC
+#gYEAz+hbqqnE5BSW0THf7txxsJxF8Vtca2uL52iutI1SRTm9J6NNtAjgMbL9upOm
+#SFnObpWKriUIlvxKrecygGWkjiMeyU/F6auAS9/vwDdxYEVT2szK+Q2At1FgU433
+#Pds53v2J/vyB6SL+k/w54H2gF4ORpU1hjUggo7fM353TeeMCAwEAAaAAMA0GCSqG
+#SIb3DQEBBAUAA4GBAIYvVThVeocN7N7HbsO/au9AXnx6LULQ5LMDWx6FlyBB5g9h
+#5HYZa6xieYCYDxYIsjLjR3qx1BWl9+0kSL2MW4EdDPzbcrZvHAtrw2/hPrm9EGA3
+#2w3a26W79N3clCkrahnpcNFLFyzU3CtZASJ+VuixGXTEkdiBAliqtGp+QBhf -----END
+#CERTIFICATE REQUEST-----)
+#parseCRS = {action: "PARSE_CSR", object: "TRUST_SERVICE", product_type: "quickssl", csr: sslcert }
+#
+#registerSSL = {action: "SW_REGISTER", object: "TRUST_SERVICE", reg_type: "new", product_type: "quickssl"}
+#renewSSL = {action: "SW_REGISTER", object: "TRUST_SERVICE", reg_type: "renew", product_id: "12"}
+#renewSSL = {action: "SW_REGISTER", object: "TRUST_SERVICE", reg_type: "renew", order_id: "12"}
+#renewSSL = {action: "SW_REGISTER", object: "TRUST_SERVICE", reg_type: "renew", inventory_item_id: "12"}
+
 
 action = "SW_REGISTER"
 object = "TRUST_SERVICE"
