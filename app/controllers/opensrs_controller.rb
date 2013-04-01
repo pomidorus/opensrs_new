e45a81d5dcd6a1a467f9b7de551ed92886673e68
andrus <andrewaka39@gmail.com>
2013-04-01 12:17:07 +0300

    clear

--- app/controllers/opensrs_controller.rb
+++ app/controllers/opensrs_controller.rb
@@ -1,65 +1,6 @@
 require 'nokogiri'
 require 'opensrs'
 
-ssl_certificate = {
-  :nomenclature_id => :integer,
-
-  :csr => :text,
-
-  :common_name => :string,
-  :validation_email => :string,
-
-  :additional_domains => :text,
-  :additional_validation_emails => :text,
-
-  :country => :string,
-  :zip => :string,
-  :stateorprovincename => :string,
-  :city => :string,
-  :street1 => :string,
-  :street2 => :string,
-  :organisation_name => :string,
-  :organisation_unit => :string,
-
-  :server_count => :integer,
-  :domain_count => :integer,
-  :ssl_server_software_id => :integer,
-
-  :duration => :integer,
-  :valid_from => :date,
-  :valid_to => :date,
-
-  :issuer_order_number => :string,
-  :provider_order_number => :string,
-  :serial_number => :string,
-
-  :certificate_state => :string,
-  :issuer_order_date => :datetime,
-  :issuer_order_state => :string,
-  :issuer_order_state_additional => :string, #только для Comodo
-  :issuer_order_state_minor_code => :string, #только для Comodo
-  :issuer_order_state_minor_name => :string, #только для Comodo
-
-  :issued_dt => :datetime,
-  :our_sell_price => :decimal,
-  :our_sell_currency => :string,
-
-  :admin_contact_person_id => :integer,
-  :tech_contact_person_id => :integer,
-  :callback_contact_person_id => :integer,
-
-  :approver_notified_date => :datetime,
-  :approver_confirm_date => :datetime,
-
-  :organisation_phone => :string,
-  :organisation_fax => :string,
-
-  :provider_id => :integer,
-
-  :company_number => :string,
-  :dcv_method => :string,
-}
-
 # empty class for client class SslProxy
 class SslProxy
   # empty
@@ -84,68 +25,6 @@ class OpenSRSClient < SslProxy
       @response.response
     end
 
-
-    #def renew_an_order_to_upgrade(attributes)
-    #  {
-    #    :domain_name => 'example.com',
-    #    :order_id => 5597,
-    #    :state => 'awaiting-approval'
-    #  }
-    #end
-    #
-    #def get_renew_ssl_a_new_order_for_a_quickssl_certificate_based_on_an_existing_order(base_order_id, csr, admin)
-    #  {
-    #    :domain_name => 'example.com',
-    #    :order_id => 8279,
-    #    :state => 'awaiting-approval'
-    #  }
-    #end
-    #
-    #def get_renew_ssl_an_order_for_a_30_day_free_trial_of_a_symantec_securesite_certificate(attributes)
-    #  {
-    #    :domain_name => "example.com",
-    #    :order_id => 7737,
-    #    :state => "awaiting-approval"
-    #  }
-    #end
-    #
-    #def get_renew_ssl_an_order_for_a_geotrust_web_site_anti_malware_scan_certificate(attributes)
-    #  {
-    #    :domain_name => "example.com",
-    #    :order_id => 6854,
-    #    :state => "awaiting-approval"
-    #  }
-    #end
-    #
-    #def get_renew_ssl_a_renewal_order_for_a_quickssl_certificate_that_was_submitted_by_using_the_order_id(attributes)
-    #  {
-    #    :domain_name => "example.com",
-    #    :order_id => attributes["order_id"],
-    #    :state => "awaiting-approval"
-    #  }
-    #end
-    #
-    #def get_renew_ssl_a_renewal_order_for_a_quickssl_certificate_that_was_submitted_by_using_the_product_id(attributes)
-    #  {
-    #    :domain_name => "certtest.example.org",
-    #    :order_id => 8310,
-    #    :state => "awaiting-approval"
-    #  }
-    #end
-    #
-    #def get_renew_ssl_renewal_order_for_a_quickssl_certificate(attributes)
-    #  {
-    #    :domain_name => "example.com",
-    #    :order_id => 8321,
-    #    :state => "awaiting-approval"
-    #  }
-    #
-    #end
-
-    #def csr
-    #  '-----BEGIN CERTIFICATE REQUEST----- MIIC2jCCAcICAQAwgZQxITAfBgNVBAMTGHRydWViaXoucWFyZWdyZXNzaW9uLm9y ZzELMAkGA1UEBhMCQ0ExCzAJBgNVBAgTAk9OMRAwDgYDVQQHEwdUb3JvbnRvMQ 8w DQYDVQQKEwZUdWNvd3MxEDAOBgNVBAsTB1FBIERlcHQxIDAeBgkqhkiG9w0BCQE WEXFhZml2ZUB0dWNvd3MuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC AQEAo+4AzMq3JeXV5KlAD3BBOGdAOuJYBW3Bz1BooLPX4MGefxqzfVcR8KLGg5MS PLqdiY4Sqc+/tK8qabpHttdbAZ1WBvgYmviMkhRjpSrbVjOca0CmydPCVsXu5nnE HMEZODrzhpuHHIzrkclBpGAqEhf9v1g4OFt1sInVB0o8NpeT10aFyvX2HbtsJyfZ S4RMsP+XjVWzWZ+8v2bH6gapJ0tzXvTKwXzhUzElvVqpldpzO0FgnJtHmfJ/EOs5 gntzVIxzP12ZKFf0dYYUj0OKWU+aQodlic2oVxETyWKCoX5W7jQgpTV/vAF7nQY8 Y9VtV6SE5yQRYPJutDTk2PouEwIDAQABoAAwDQYJKoZIhvcNAQEEBQADggEBAAUr DUNxyrYpt3t9r0GCIiIDVyQdJvY4tQUFIEJdxcvRo2TUcrgiWPyntGc1OCtUFE9Z 2JX4BNEmFVN1jUdBzh6/0loAA36iGYWTSB6CPVe5+y+dcgbViWcNV4or7FOslzRH /Eu0CquMGmGtSdaT/DNIrJvM2iGOtuhFBhFyru61YMoeaQLU12i5XvK7bR4wHrG6 8DwlwUdzBRqiaq32rM/ZF2KmMzfLFKug1Hubt3OBQHSKwXz3CR7hrJSzf1q3lF/w HD47TC982HXaUuskI+E0LcuR/qprLkvAO6hKT60CP+V/yNwcBu79Zjeg1MsAmH/W SzFmc1swYutlFBxmyLU= -----END CERTIFICATE REQUEST-----'
-    #end
-
   end
 
 
@@ -165,35 +44,8 @@ class OpenSRSRequestParse
     request.xpath('//OPS_envelope/dt_assoc/item/dt_assoc/item').each do |item|
       rh[item['key']] = item.content
     end
-    #request.xpath('//OPS_envelope/dt_assoc/item/dt_assoc/item').each do |item|
-    #  rh[item['key']] = item.content
-    #end
-    #rh['full'] = xml_node_to_hash(request.root)
     rh
   end
-
-  #def xml_node_to_hash(node)
-  #  # If we are at the root of the document, start the hash
-  #  if node.element?
-  #
-  #      if node.children.count == 1 && node.children.first.name.to_s == 'text'
-  #        return node.children.first.content.to_s
-  #      end
-  #
-  #      result_hash = {}
-  #      node.children.each do |child|
-  #        result = xml_node_to_hash(child)
-  #        if child['key'].blank?
-  #          result_hash[child.name.to_s] = result
-  #        else
-  #          result_hash[child['key']] = result
-  #        end
-  #      end
-  #      return result_hash
-  #  else
-  #    return node.content.to_s
-  #  end
-  #end
 end
 
 class OpenSRSResponse
@@ -215,6 +67,8 @@ class OpenSRSResponse
   RESEND_APPROVE_EMAIL = "RESEND_APPROVE_EMAIL"
   RESEND_CERT_EMAIL = "RESEND_CERT_EMAIL"
   SW_REGISTER = "SW_REGISTER"
+  CANCEL_ORDER = "CANCEL_ORDER"
+  PARSE_CSR = "PARSE_CSR"
 
   QUERY_APPROVER_LIST_RESPONSE = "approver_list_response"
   RESEND_APPROVE_EMAIL_RESPONSE = "resend_approve_email"
@@ -309,7 +163,6 @@ class OpenSRSResponse
   def approver_list(domain, product_type)
     #client_function(domain, product_type)
     #response client function
-    p "domain #{domain}"
     {
       approver_list: [
         {email: "qafive@example.com", domain: "example.com", type: "MANUAL"},
@@ -324,7 +177,6 @@ class OpenSRSResponse
   def resend_approve_email(order_id)
     #client_function(order_id)
     #response client function
-    p "order_id #{order_id}"
     {
       order_id: "1111"
     }
@@ -333,7 +185,6 @@ class OpenSRSResponse
   def resend_cert_email(order_id)
     #client_function(order_id)
     #response client function
-    p "order_id #{order_id}"
     {
       order_id: "2222"
     }
@@ -342,7 +193,6 @@ class OpenSRSResponse
   def product_info(product_id)
     #client_function(product_id)
     #response client function
-    p "product_id #{product_id}"
     {
       product_type: "truebizid_wildcard",
       issue_date: "2010-09-14-04:00",
@@ -353,25 +203,12 @@ class OpenSRSResponse
       expiry_date: "2010-09-22-04:00",
       is_renewable: "0",
       state: "expired"
-      #:code => :string,
-      #:name => :string,
-      #:price => :decimal,
-      #:certificate_type => %w{standart wildcard ucc code_signing},
-      #:validation_type => %w{ov dv},
-      #:is_ev => :boolean,
-      #:is_sgc => :boolean,
-      #:issuer_organization_name => :string,
-      #:is_free => :boolean,
-      #:discontinued => :boolean,
-      #:is_email_validated => :boolean,
-      #:domain_name => 'example.ru'
     }
   end
 
   def order_info(order_id)
     #client_function(order_id)
     #response client function
-    p "order_id #{order_id}"
     {
      owner: {
               first_name: "Andrey",
@@ -584,42 +421,10 @@ class OpenSRSResponse
     when SW_REGISTER
       result[:data], result[:layout] = register_ssl(order_id)
 
-        #if reg_type == "upgrade"
-        #  result[:data] = item_open_srs_client.renew_an_order_to_upgrade(@request_hash["full"]["dt_assoc"]["attributes"]["dt_assoc"])
-        #  result[:layout] = "renew_an_order_to_upgrade_a_sitelock_ssl_certificate_to_sitelock_premium"
-        #
-        #elsif reg_type == "new" && product_type == "quickssl"
-        #  result[:data] = item_open_srs_client.get_renew_ssl_a_new_order_for_a_quickssl_certificate_based_on_an_existing_order(@request_hash["base_order_id"],@request_hash["csr"],@request_hash["full"]["dt_assoc"]["attributes"]["dt_assoc"]["contact_set"]["dt_assoc"]["admin"]["dt_assoc"])
-        #  result[:layout] = "renew_a_new_order_for_a_quickssl_certificate_based_on_an_existing_order"
-        #
-        #elsif reg_type == "new" && product_type == "securesite_ft"
-        #  result[:data] = item_open_srs_client.get_renew_ssl_an_order_for_a_30_day_free_trial_of_a_symantec_securesite_certificate(@request_hash["full"]["dt_assoc"]["attributes"]["dt_assoc"])
-        #  result[:layout] = "renew_an_order_for_a_30_day_free_trial_of_a_symantec_securesite_certificate"
-        #
-        #elsif reg_type == "new" && product_type == "malwarescan"
-        #  result[:data] = item_open_srs_client.get_renew_ssl_an_order_for_a_geotrust_web_site_anti_malware_scan_certificate(@request_hash["full"]["dt_assoc"]["attributes"]["dt_assoc"])
-        #  result[:layout] = "renew_an_order_for_a_geotrust_web_site_anti_malware_scan_certificate"
-        #
-        #elsif reg_type == "renew" && !order_id.blank?
-        #  result[:data] = item_open_srs_client.get_renew_ssl_a_renewal_order_for_a_quickssl_certificate_that_was_submitted_by_using_the_order_id(@request_hash["full"]["dt_assoc"]["attributes"]["dt_assoc"])
-        #  result[:layout] = "renew_a_renewal_order_for_a_quickssl_certificate_that_was_submitted_by_using_the_order_id"
-        #
-        #elsif reg_type == "renew" && !inventory_item_id.blank?
-        #  result[:data] = item_open_srs_client.get_renew_ssl_a_renewal_order_for_a_quickssl_certificate_that_was_submitted_by_using_the_product_id(@request_hash["full"]["dt_assoc"]["attributes"]["dt_assoc"])
-        #  result[:layout] = "renew_a_renewal_order_for_a_quickssl_certificate_that_was_submitted_by_using_the_product_id"
-        #
-        #elsif reg_type == "renew" && !product_id.blank?
-        #  result[:data] = item_open_srs_client.get_renew_ssl_renewal_order_for_a_quickssl_certificate(@request_hash["full"]["dt_assoc"]["attributes"]["dt_assoc"])
-        #  result[:layout] = "renew_renewal_order_for_a_quickssl_certificate"
-        #else
-        #  result[:data] = item_open_srs_client.register_ssl_cert(@request_hash["order_id"])
-        #  result[:layout] = "register_ssl_cert_response"
-        #end
-
-    when "CANCEL_ORDER"
+    when CANCEL_ORDER
         result[:data], result[:layout] = cancel_order(order_id), "cancel_order_response"
 
-    when "PARSE_CSR"
+    when PARSE_CSR
         result[:data], result[:layout] = parse_csr(product_type, crs), "parse_csr_response"
     end
 
@@ -642,11 +447,8 @@ class OpensrsController < ApplicationController
     opensrs = OpenSRSClient.new(opensrs_request_hash,username,signature)
 
     if opensrs.authenticate?
-      #command = OpenSRSResponse.new(opensrs_request_hash).response(opensrs)
-      response_hash = opensrs.response # at this moment always empty
-      # data for layout
+      response_hash = opensrs.response
       @data = response_hash[:data]
-      p @data
       render "layouts/#{response_hash[:layout]}", :formats => [:xml]
     else
       render "layouts/bad_authorization", :formats => [:xml]
@@ -654,27 +456,3 @@ class OpensrsController < ApplicationController
   end
 end
 
-  #before_filter :authenticate_user
-    #opensrs_response = OpenSRSResponse.new(opensrs_request)
-    #client_data = opensrs_response.client_data
-
-
-    #puts opensrs_response.at_css("item").text
-    #attributes = {:foo => {:bar => 'baz'}}
-    #xxml = OpenSRS::XmlProcessor::Nokogiri.build(attributes)
-    #puts xxml
-    #render :xml => xml_body
-    #OpenSRS::Server.xml_processor = :nokogiri
-
-    # Rails.logger.debug "#{authenticate_client_function(username,signature)}"
-  #private
-  #def authenticate_user
-  #  username = request.headers["X-Username"]
-  #  signature = request.headers["X-Signature"]
-  #  @current_user = User.find_by_signature(signature)
-  #    #unless @current_user
-  #    #  respond_with({:error => "Signature is invalid." })
-  #    #end
-  #end
-    # Rails.logger.debug "#{opensrs_request.inspect}"
-    # Rails.logger.debug "#{opensrs_response.response.inspect}"
